66318b2cc8df9118b02715748a6605271fabf950
lichao-mobanche <494232356@qq.com>
2017-09-02 19:12:35 +0800

    zidonghububushu

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ _grafana_version=${_grafana_tag:1}
 if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
 	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
+	https://github.com/lichao-mobanche/grafana/releases/download/v4.4.4/grafana_4.4.3_amd64.deb
+
 	docker build \
 		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
 		--tag "grafana/grafana:${_grafana_version}" \
