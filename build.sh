ef5b841d2c6ad2289ec05faae507cb0dc4979805
lichao-mobanche <494232356@qq.com>
2017-09-04 12:34:19 +0800

    自动化部署

--- build.sh
+++ build.sh
@@ -5,19 +5,14 @@ _grafana_version=${_grafana_tag:1}
 
 if [ "$_grafana_version" != "" ]; then
 	echo "Building version ${_grafana_version}"
-	echo "Download url: https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb"
-	https://github.com/lichao-mobanche/grafana/releases/download/v4.4.4/grafana_4.4.3_amd64.deb
-
+	echo "Download url: https://github.com/laincloud/grafana/releases/download/${_grafana_tag}/grafana_${_grafana_version}_amd64.deb"
+	
 	docker build \
-		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_${_grafana_version}_amd64.deb \
-		--tag "grafana/grafana:${_grafana_version}" \
+		--build-arg DOWNLOAD_URL=https://github.com/laincloud/grafana/releases/download/${_grafana_tag}/grafana_${_grafana_version}_amd64.deb \
+		--tag "laincloud/grafana:${_grafana_version}" \
 		--no-cache=true .
-	docker tag grafana/grafana:${_grafana_version} grafana/grafana:latest
+	docker tag laincloud/grafana:${_grafana_version} laincloud/grafana:latest
 
 else
-	echo "Building latest for master"
-	docker build \
-		--build-arg DOWNLOAD_URL=https://s3-us-west-2.amazonaws.com/grafana-releases/master/grafana_latest_amd64.deb \
-		--tag "grafana/grafana:master" \
-		--no-cache=true .
+	echo "please input the version"
 fi
--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -4,10 +4,9 @@ _grafana_tag=$1
 _grafana_version=${_grafana_tag:1}
 
 if [ "$_grafana_version" != "" ]; then
-	echo "pushing grafana/grafana:${_grafana_version}"
-	docker push grafana/grafana:${_grafana_version}
-	docker push grafana/grafana:latest
+	echo "pushing laincloud/grafana:${_grafana_version}"
+	docker push laincloud/grafana:${_grafana_version}
+	docker push laincloud/grafana:latest
 else
-	echo "pushing grafana/grafana:master"
-	docker push grafana/grafana:master
+	echo "please input the version"
 fi
